626e95566c9e13b8005683e858f41c0943683923
Divyansh Jindal <ninjsa@hotmail.com>
2024-11-21 12:37:27 +0530

    Update GameFrame.java

--- FlappyBird/GameFrame.java
+++ FlappyBird/GameFrame.java
@@ -1,1 +1,17 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    public GameFrame() {
+        this.setTitle(" Divyansh Flappy Bird");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.add(new GamePanel());
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
 
